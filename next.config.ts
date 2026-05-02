4f23371c2bc5e0e5501611706218fac4d8889301
blackbearlabs888 <rukopalangkaraya.3sco@gmail.com>
2026-05-02 18:39:55 +0700

    z

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   typescript: {
     // DO NOT ignore build errors in production
     ignoreBuildErrors: false,
   },
   reactStrictMode: false,
-  turbopack: {},
 };
 
 export default nextConfig;
